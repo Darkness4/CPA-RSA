edb2cbb3c214619b786bb062c718eba57529bc07
Nguyen Marc <nguyen_marc@live.fr>
2021-02-01 16:59:08 +0100

    up

--- CPA_RSA.py
+++ CPA_RSA.py
@@ -4,6 +4,7 @@ Main Program.
 Decrypt RSA private key using Correlation Power Analysis.
 """
 
+import matplotlib.pyplot as plt
 import numpy as np
 from typing import Iterable, Tuple
 from functools import reduce
@@ -14,7 +15,7 @@ ASSETS_PATH: str = os.path.join(os.path.dirname(__file__), "assets")
 MODULO_PATH: str = os.path.join(ASSETS_PATH, "N.txt")
 CURVE_FILE_NAMING_PATTERN: str = "curve_{:d}.txt"
 PLAINTEXT_FILE_NAMING_PATTERN: str = "msg_{:d}.txt"
-PUBLIC_KEY: int = 65537
+PUBLIC_KEY: int = 2 ** 16 + 1
 
 with open(MODULO_PATH, "r") as file:
     N = int(file.readline())
@@ -57,10 +58,12 @@ class NumberUtils:
             old_r, r = r, old_r - q * r
             old_t, t = t, old_t - q * t
 
-        return old_t if (old_t >= 0) else b - old_t
+        return old_t if (old_t >= 0) else b + old_t
 
     @staticmethod
-    def hamming_weight_for_rsa(data: int, exponent_bin: Iterable[int], n: int) -> int:
+    def hamming_weight_for_rsa(
+        data: int, exponent_bin: Iterable[int], n: int
+    ) -> int:
         """Calculate key with Square-And-Multiply, and compute Hamming Weight."""
         result = 1
         for bit in reversed(exponent_bin):
@@ -80,16 +83,18 @@ class NumberUtils:
         return ((B.T).dot(A) / B.shape[0])[0]
 
     @staticmethod
-    def bitListToInt(bits: Iterable[int]) -> int:
+    def bit_list_to_int(bits: Iterable[int]) -> int:
         """Convert a list of bit to an integer."""
         return reduce(lambda acc, value: acc * 2 + value, bits)
 
 
 def load_power_consumptions(
-    number_of_trace: int = 1000, number_of_points_per_trace: int = 36
+    number_of_trace: int, number_of_points_per_trace: int
 ) -> np.ndarray:
     """Load the traces stored in files."""
-    power_consumptions = np.zeros((number_of_trace, number_of_points_per_trace))
+    power_consumptions = np.zeros(
+        (number_of_trace, number_of_points_per_trace)
+    )
     for i in range(number_of_trace):
         filename = CURVE_FILE_NAMING_PATTERN.format(i)
         pathfile = os.path.join(ASSETS_PATH, filename)
@@ -100,11 +105,19 @@ def load_power_consumptions(
                 lambda x: x != -1000.0,
                 map(float, file.readline().rstrip().split()),
             )
-            power_consumptions[i][:] = np.fromiter(data_iter, float)
+            data = np.fromiter(data_iter, float)
+            if number_of_points_per_trace != np.size(data):
+                print(
+                    f"WARNING: {number_of_points_per_trace} points used on {np.size(data)} total data\n"
+                    f"You may want to increase [number_of_points_per_trace] by {np.size(data)-number_of_points_per_trace}"
+                )
+            power_consumptions[i][:number_of_points_per_trace] = data[
+                :number_of_points_per_trace
+            ]
     return power_consumptions
 
 
-def fetch_plaintexts(number_of_plaintext: int = 1000) -> Iterable[int]:
+def fetch_plaintexts(number_of_plaintext: int) -> Iterable[int]:
     """Load the plaintexts."""
     for i in range(number_of_plaintext):
         filename = PLAINTEXT_FILE_NAMING_PATTERN.format(i)
@@ -129,18 +142,27 @@ def compute_hypothesis_matrix(
             hypothesis_key = (hypothesis_bit,) + old_key
             hypothesis_matrix[
                 plaintext_idx, hypothesis_bit
-            ] = NumberUtils.hamming_weight_for_rsa(plaintext, hypothesis_key, N)
+            ] = NumberUtils.hamming_weight_for_rsa(
+                plaintext, hypothesis_key, N
+            )
     return hypothesis_matrix
 
 
 def compute_key_by_factoring() -> int:
     """Calculate the private key using prime number factoring."""
     p, q = NumberUtils.prime_factors(N)
+    if p * q != N:
+        raise ValueError("p * q != N")
     totient = (p - 1) * (q - 1)
+
     return NumberUtils.mod_inverse(PUBLIC_KEY, totient)
 
 
-def compute_key_by_cpa(number_of_plaintext=1000, number_of_points_per_trace=36) -> int:
+def compute_key_by_cpa(
+    number_of_plaintext,
+    number_of_points_per_trace,
+    enable_plot=False,
+) -> int:
     """Calculate the private key using CPA.
 
     The aim of this function is to build the private key gradually, bit by bit.
@@ -149,11 +171,18 @@ def compute_key_by_cpa(number_of_plaintext=1000, number_of_points_per_trace=36) 
 
     Based on the correlation matrix, we select the best hypothesis.
     """
-    plaintexts = tuple(fetch_plaintexts(number_of_plaintext=number_of_plaintext))
+    plaintexts = tuple(
+        fetch_plaintexts(number_of_plaintext=number_of_plaintext)
+    )
     power_consumptions = load_power_consumptions(
         number_of_trace=number_of_plaintext,
         number_of_points_per_trace=number_of_points_per_trace,
     )  # shape (1000, 36)
+
+    # Plot Variables
+    x: np.ndarray = np.arange(number_of_points_per_trace)
+    corr1: np.ndarray = np.zeros(number_of_points_per_trace)
+    corr0: np.ndarray = np.zeros(number_of_points_per_trace)
 
     # Key start with 1 for obvious reasons. Key is little-endian.
     key_builder = [1]
@@ -169,19 +198,61 @@ def compute_key_by_cpa(number_of_plaintext=1000, number_of_points_per_trace=36) 
             hypothesis_matrix, power_consumption_of_bit
         )  # shape (2,)
 
+        corr0[time] = correlation_matrix[0]
+        corr1[time] = correlation_matrix[1]
+        print(f"acc = {max(correlation_matrix[0], correlation_matrix[1])}")
+        if abs(correlation_matrix[0] - correlation_matrix[1]) < 0.1:
+            print("WARNING: Uncertain bit !")
         best_hypothesis = np.argmax(correlation_matrix)
 
         key_builder.insert(0, best_hypothesis)  # .prepend
         if best_hypothesis == 1:  # If new bit is Square-Multiply.
-            time += 1  # Skip Multiply to avoid counting the same operation.
+            time += 1  # Skip to avoid counting the same operation.
         time += 1
 
-    return NumberUtils.bitListToInt(reversed(key_builder))
+    # Plot
+    if enable_plot:
+        bar0 = plt.bar(
+            x,
+            corr0,
+            width=0.4,
+            align="edge",
+            label="Hypothesis 0",
+        )
+        plt.bar(
+            x + 0.4,
+            corr1,
+            width=0.4,
+            align="edge",
+            label="Hypothesis 1",
+        )
+
+        bar0_filtered = tuple(filter(lambda x: x.get_height() > 0.01, bar0))
+        plt.text(0.0, 0.5, str(key_builder[-1]))
+        for rect, bit in zip(bar0_filtered, reversed(key_builder[:-1])):
+            plt.text(rect.get_x(), 0.5, str(bit))
+        plt.legend()
+        plt.xlabel("Time")
+        plt.ylabel("Correlation Coefficient")
+        plt.title(
+            "Histogram of Correlation Coefficient by time and hypothesis"
+        )
+
+        plt.show()
+
+    return NumberUtils.bit_list_to_int(reversed(key_builder))
 
 
 if __name__ == "__main__":
+    number_of_plaintext = 1000
+    number_of_points_per_trace = 36
+    enable_plot = False
     expected = compute_key_by_factoring()
-    result = compute_key_by_cpa()
+    result = compute_key_by_cpa(
+        number_of_plaintext=number_of_plaintext,
+        number_of_points_per_trace=number_of_points_per_trace,
+        enable_plot=enable_plot,
+    )
     print(f"Factoring : {expected:b}")
     print(f"CPA : {result:b}")
     print(f"Equality : {result == expected}")
